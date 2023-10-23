760d8d060ada2208fb588bfb74251b8ad133e11e
Sergiu IT <itsergiu@yahoo.com>
2023-10-23 12:28:10 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,123 @@
+# AI Challenge | Web scrapping with Generative AI!
+# Blog:
+# Online app:
+# Created by: Sergiu Iatco | October, 2023
+# https://www.linkedin.com/in/sergiuiatco/
+
+import pandas as pd
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+from utility_settings import SaveResultsCsvJson
+from sap_blog_statistics import SapBlogStatistics
+
+
+def f_init_session():
+    if 'df_file' not in st.session_state:
+        st.session_state['df_file'] = pd.DataFrame()
+    if 'df_results' not in st.session_state:
+        st.session_state['df_results'] = pd.DataFrame()
+    if 'df_ok_sum' not in st.session_state:
+        st.session_state['df_ok_sum'] = pd.DataFrame()
+    if 'df_ok_count' not in st.session_state:
+        st.session_state['df_ok_count'] = pd.DataFrame()
+    if 'df_ok' not in st.session_state:
+        st.session_state['df_ok'] = pd.DataFrame()
+
+
+def f_web_scrapping(p_file_name):
+    bls = SapBlogStatistics(p_file_name)
+    bls.mt_iter_file()
+    bls.mt_rename_cols(['url', 'OK', 'comments', 'likes', 'views'])
+    bls.mt_convert_cols_to_int(['comments', 'likes', 'views'])
+    df_file = bls.df_file
+    df_results = bls.df_results
+    ok_dict = bls.mt_calculate_totals()
+    df_ok_sum = ok_dict['df_ok_sum']
+    df_ok_count = ok_dict['df_ok_count']
+    return df_file, df_results, df_ok_sum, df_ok_count, ok_dict
+
+
+def f_download_button(p_df):
+    lc_obj_save = SaveResultsCsvJson(p_df)
+    lc_obj_save.generate_json_text_files()
+    return lc_obj_save
+
+
+def f_extract(p_file_name):
+    df_file, df_results, df_ok_sum, df_ok_count, df_ok = f_web_scrapping(p_file_name)
+    st.session_state['df_file'] = df_file
+    st.session_state['df_results'] = df_results
+    st.session_state['df_ok_sum'] = df_ok_sum
+    st.session_state['df_ok_count'] = df_ok_count
+    st.session_state['df_ok'] = df_ok
+
+
+f_init_session()
+
+st.title("SAP Blog Statistics")
+st.write('AI Challenge | Web scrapping with Generative AI! 🤖')
+st.write('Is it possible? Share the solution! 🤖')
+st.write('For now, you can collect statistics with Python code. 🐍 😊')
+
+bt_side_sel = st.sidebar.selectbox("Select an option",
+                                   ['SAP AI Gen State',
+                                    'SAP HANA ML Cloud Challenge 2022',
+                                    'Upload SAP blog list'])
+
+with st.sidebar:
+    add_vertical_space(1)
+    st.markdown(
+        '📖 Read the [blog](https://blogs.sap.com/2023/10/02/state-of-genai-in-the-sap-community-09-2023/)')
+
+st.write(f'**{bt_side_sel}**')
+if bt_side_sel == 'Upload SAP blog list':
+    uploaded_file_disabled = False
+else:
+    uploaded_file_disabled = True
+
+file_name = None
+if bt_side_sel == 'SAP AI Gen State':
+    file_name = 'sap_blog_url_ai_state_20231002.csv'
+
+elif bt_side_sel == 'SAP HANA ML Cloud Challenge 2022':
+    file_name = 'sap_blog_url_hana_ml_challenge_202211.csv'
+
+uploaded_file = st.file_uploader("Choose a file", disabled=uploaded_file_disabled)
+
+if uploaded_file is not None:
+    file_name = uploaded_file
+
+bt_extract_disabled = False
+if bt_side_sel == 'Upload SAP blog list':
+    if uploaded_file is None:
+        bt_extract_disabled = True
+
+
+st.button("Extract Statistics", disabled=bt_extract_disabled, on_click=f_extract, args=(file_name,), key='bt_extract')
+
+obj_save = f_download_button(st.session_state['df_results'])
+
+# st.write('List of Blogs')
+# st.dataframe(st.session_state['df_file'], column_config={"url": st.column_config.LinkColumn("URL",
+#                                                                                             help="Click 🎈"),
+#                                                          })
+st.write('Blog Statistics')
+st.dataframe(st.session_state['df_results'], column_config={"url": st.column_config.LinkColumn("URL",
+                                                                                               width='large',
+                                                                                               help="Click 🎈"),
+                                                            })
+st.write('Statistics Total')
+st.dataframe(st.session_state['df_ok_sum'])
+st.write('Blog Reading Success Y/N')
+st.dataframe(st.session_state['df_ok_count'])
+
+col1, col2, = st.columns(2)
+with col1:
+    st.download_button(label="Download CSV",
+                       data=obj_save.text,
+                       file_name=obj_save.text_file)
+
+with col2:
+    st.download_button(label="Download JSON",
+                       data=obj_save.json,
+                       file_name=obj_save.json_file)
---
+++ sap_blog_statistics.py
@@ -0,0 +1,164 @@
+# Blog:
+# Online app:
+# Statistics for https://blogs.sap.com/ | comments, likes and views
+# Created by: Sergiu Iatco | October, 2023
+# https://www.linkedin.com/in/sergiuiatco/
+
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# class SapBlogStatistics:
+#     def __init__(self, file_name=None):
+#         self.file_name = file_name
+#         self.df_results = pd.DataFrame()
+#         self.df_file = pd.DataFrame()
+#
+#         if file_name is not None:
+#             self.df_file = pd.read_csv(self.file_name)
+#
+#     def mt_iter_file(self):
+#         if not self.df_file.empty:
+#             for index, row in self.df_file.iterrows():
+#                 url = row['url']
+#                 soup, ok = self.mt_requests_get(url)
+#                 result_dict = self.mt_span_elements(url, soup, ok)
+#                 self.mt_append_results(result_dict)
+#
+#     @staticmethod
+#     def mt_requests_get(url):
+#         soup, ok = None, 'N'
+#         try:
+#             response = requests.get(url)
+#             if response.status_code == 200:
+#                 soup = BeautifulSoup(response.text, 'html.parser')
+#                 ok = 'Y'
+#         except requests.RequestException:
+#             ok = 'N'
+#         return soup, ok
+#
+#     @staticmethod
+#     def mt_span_elements(url, soup, ok):
+#         result_dict = dict()
+#         result_dict['url'] = [url]
+#         result_dict['OK'] = [ok]
+#
+#         if soup is not None:
+#             # Find all span elements within the container
+#             span_elements = soup.find_all('span', class_='ds-social-stats__stat')
+#
+#             # Extract and store the data in the dictionary
+#             for span_element in span_elements:
+#                 i_element = span_element.find('i')
+#                 i_class_name = i_element['class'][1]  # Get the second class name
+#                 number_text = span_element.get_text(strip=True)
+#                 result_dict[i_class_name] = [number_text]
+#
+#         return result_dict
+#
+#     def mt_append_results(self, result_dict):
+#         df_request = pd.DataFrame.from_dict(result_dict)
+#         self.df_results = pd.concat([self.df_results, df_request], axis=0).reset_index(drop=True)
+#         self.df_results.fillna('0', inplace=True)  # replace NaN
+#
+#     def mt_rename_cols(self, new_column_names):
+#         if not self.df_results.empty:
+#             column_mapping = {old_col: new_col for old_col, new_col in zip(self.df_results.columns, new_column_names)}
+#             self.df_results.rename(columns=column_mapping, inplace=True)
+#
+#     def mt_convert_cols_to_int(self, column_names):
+#         if not self.df_results.empty:
+#             for cols in column_names:
+#                 if self.df_results.dtypes[cols] == 'object':
+#                     self.df_results[cols] = pd.to_numeric(self.df_results[cols].str.replace(',', ''))
+#                     self.df_results[cols] = pd.to_numeric(self.df_results[cols], errors='coerce', downcast='integer')
+#
+#     def mt_calculate_totals(self, cols_drop=None):
+#         if not self.df_results.empty:
+#             if cols_drop is None:
+#                 cols_drop = ['url']
+#             df = self.df_results.copy()
+#             df.drop(cols_drop, axis=1, inplace=True)
+#             df_ok_sum = df.groupby(['OK']).sum()
+#             df_ok_count = df.groupby(['OK']).size().reset_index(name='Count')
+#         else:
+#             df_ok_sum = pd.DataFrame()
+#             df_ok_count = pd.DataFrame()
+#         return dict({'df_ok_sum': df_ok_sum, 'df_ok_count': df_ok_count})
+
+class SapBlogStatistics:
+    ZERO = '0'
+    OK_SUCCESS = 'Y'
+    OK_FAILURE = 'N'
+
+    def __init__(self, file_name=None):
+        self.file_name = file_name
+        self.df_results = pd.DataFrame()
+        self.df_file = pd.DataFrame()
+
+        if file_name is not None:
+            self.df_file = pd.read_csv(file_name)
+
+    def mt_iter_file(self):
+        if not self.df_file.empty:
+            for _, row in self.df_file.iterrows():
+                url = row['url']
+                soup, ok = self.mt_requests_get(url)
+                result_dict = self.mt_span_elements(url, soup, ok)
+                self.mt_append_results(result_dict)
+
+    def mt_requests_get(self, url):
+        soup, ok = None, self.OK_FAILURE
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                ok = self.OK_SUCCESS
+        except requests.RequestException:
+            ok = self.OK_FAILURE
+        return soup, ok
+
+    @staticmethod
+    def mt_span_elements(url, soup, ok):
+        result_dict = {'url': [url], 'OK': [ok]}
+
+        if soup is not None:
+            span_elements = soup.find_all('span', class_='ds-social-stats__stat')
+            for span_element in span_elements:
+                i_element = span_element.find('i')
+                i_class_name = i_element['class'][1]
+                number_text = span_element.get_text(strip=True)
+                result_dict[i_class_name] = [number_text]
+
+        return result_dict
+
+    def mt_append_results(self, result_dict):
+        df_request = pd.DataFrame.from_dict(result_dict)
+        self.df_results = pd.concat([self.df_results, df_request], axis=0).reset_index(drop=True)
+
+    def mt_rename_cols(self, new_column_names):
+        if not self.df_results.empty:
+            column_mapping = {old_col: new_col for old_col, new_col in zip(self.df_results.columns, new_column_names)}
+            self.df_results.rename(columns=column_mapping, inplace=True)
+
+    def mt_convert_cols_to_int(self, column_names):
+        if not self.df_results.empty:
+            for cols in column_names:
+                if self.df_results.dtypes[cols] == 'object':
+                    self.df_results[cols] = pd.to_numeric(self.df_results[cols].str.replace(',', ''))
+                    self.df_results[cols] = pd.to_numeric(self.df_results[cols], errors='coerce', downcast='integer')
+
+    def mt_calculate_totals(self, cols_drop=None):
+        if not self.df_results.empty:
+            if cols_drop is None:
+                cols_drop = ['url']
+            df = self.df_results.copy()
+            df.drop(cols_drop, axis=1, inplace=True)
+            df_ok_sum = df.groupby(['OK']).sum()
+            df_ok_count = df.groupby(['OK']).size().reset_index(name='Count')
+        else:
+            df_ok_sum = pd.DataFrame()
+            df_ok_count = pd.DataFrame()
+        return {'df_ok_sum': df_ok_sum, 'df_ok_count': df_ok_count}
